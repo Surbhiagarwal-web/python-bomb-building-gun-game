0f45c0cb69134087865be1f9b6c94ecef0b7f031
Surbhiagarwal-web <surbhiagarwal3116@gmail.com>
2025-09-05 09:55:26 +0000

    game

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import random
-print("Welcome to the game of rock, paper, scissors")
+print("Welcome to the game of Gun, Building, Bomb")
 
-choices = ("rock", "paper", "scissors")
+choices = ("Gun", "Building", "Bomb")
 comp = random.choice(choices)
 user = input("Enter your choice: ")
 
@@ -9,26 +9,26 @@ if comp == user:
     print("Both have entered the same choice")
     print("The game is draw")
 
-if comp == "rock" and user == "paper":
-    print("Computer chose rock")
+if comp == "Bomb" and user == "Gun":
+    print("Computer chose Bomb")
     print("You win")
 
-if comp == "rock" and user == "scissors":
-    print("Computer chose rock")
+if comp == "Bomb" and user == "Building":
+    print("Computer chose Bomb")
     print("You lost")
 
-if comp == "paper" and user == "scissors":
-    print("Computer chose paper")
+if comp == "Building" and user == "Bomb":
+    print("Computer chose Building")
     print("You win")
 
-if comp == "paper" and user == "rock":
-    print("Computer chose paper")
+if comp == "Building" and user == "Gun":
+    print("Computer chose Building")
     print("You lost")
 
-if comp == "scissors" and user == "rock":
-    print("Computer chose scissors")
-    print("You win")
-
-if comp == "scissors" and user == "paper":
-    print("Computer chose scissors")
+if comp == "Gun" and user == "Bomb":
+    print("Computer chose Gun")
     print("You lost")
+
+if comp == "Gun" and user == "Building":
+    print("Computer chose Gun")
+    print("You Win")
